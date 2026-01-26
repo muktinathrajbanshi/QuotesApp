ad73ba224032e5789961655e1919f0143ed834ce
unknown <muktinathrajbanshi786@gmail.com>
2026-01-26 16:57:55 +0545

    are wah

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
  let realData = "";
  const getNewQuotes = () => {
     let ranum = Math.floor(Math.random() * 10);
-    console.log(ranum);
-    
- }
+    console.log(realData[ranum].a);
+ };
 
 const getQuotes = async () => {
     const api = "https://zenquotes.io/api/quotes";
     const proxy = "https://api.allorigins.win/get?url=" + encodeURIComponent(api);
 
     try {
-        const data = await fetch(proxy);
+        let data = await fetch(proxy);
         const midData = await data.json();
-        const realData = JSON.parse(midData.contents);
+         realData = JSON.parse(midData.contents);
         getNewQuotes();
         // console.log(realData.length);
         // console.log(realData[0].q);
