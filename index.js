331424e340e284b83492e498cbd3d44bfc70507c
unknown <muktinathrajbanshi786@gmail.com>
2026-01-27 09:58:53 +0545

    logic part is created

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
- let realData = "";
- const getNewQuotes = () => {
-    let ranum = Math.floor(Math.random() * 10);
-    console.log(ranum[10].a);
- };
 
 const getQuotes = async () => {
-    const api = "https://zenquotes.io/api/quotes";
-    const proxy = "https://api.allorigins.win/get?url=" + encodeURIComponent(api);
-
+    
     try {
-        let data = await fetch(proxy);
-        const midData = await data.json();
-         realData = JSON.parse(midData.contents);
-         console.log(realData[10].a);
-         
-        getNewQuotes();
+        const response = await fetch("./api/index.json");
+        const data = await response.json();
+        console.log(data);
     } catch (error) {
-        console.log("Error fetching quotes:", error);
+        console.log(error);
     }
 
 };
