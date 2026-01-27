aaed9d1cefa744fc21f00684b2d4075f154e1668
unknown <muktinathrajbanshi786@gmail.com>
2026-01-27 10:20:02 +0545

    oh my god due to CROS policy it is hacked so that i build my personal API for this project

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const getQuotes = async () => {
     
     try {
-        const response = await fetch("./api/index.json");
+        const response = await fetch("https://raw.githubusercontent.com/muktinathrajbanshi/quotes-api/refs/heads/main/index.json");
         const data = await response.json();
         console.log(data);
     } catch (error) {
